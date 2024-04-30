e250e10b8549c67b62f8fd17ef5e0e4f410965a8
Ranjan Yadav <theranzanydv@gmail.com>
2024-04-30 16:40:16 +0545

    blog complete

--- app/controller/blog.controller.js
+++ app/controller/blog.controller.js
@@ -1,9 +1,26 @@
 const { eq } = require('drizzle-orm');
 const createError = require('http-errors');
+const multer = require('multer');
 
 const db = require('../db/connect');
 const { user, blog } = require('../db/schema');
 const catchAsync = require('../utils/catchAsync');
+const path = require('node:path');
+const { uuid } = require('drizzle-orm/pg-core');
+
+// Setup storage engine
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');  // Set the destination directory (adjust as needed)
+  },
+  filename: function (req, file, cb) {
+    // Create a custom filename with the original extension
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + Date.now() + ext);
+  }
+});
+// Initialize multer with the storage configuration
+const upload = multer({ storage: storage }).single('file');
 
 
 const createBlog = catchAsync(async (req, res) => {
@@ -17,25 +34,19 @@ const createBlog = catchAsync(async (req, res) => {
   if (slugExists.length > 0) {
     throw createError(409, 'Blog with slug already exists');
   }
-
-  const newBlog = await db.insert(blog).values(
-    {
-      title: title,
-      slug: slug,
-      content: content,
-      featuredImage: featuredImage ? featuredImage : '/uploads/puremotion.jpg',
-      // userId
-    }
-  ).returning({
+  const newBlog = await db.insert(blog).values({
+    title,
+    slug,
+    content,
+    featuredImage: featuredImage ? featuredImage : '/uploads/puremotion.jpg',
+  }).returning({
+    id: blog.id,
     title: blog.title,
     slug: blog.slug,
     content: blog.content,
-    featuredImage: featuredImage,
-    // userId: blog.userId,
-    createdAt: blog.createdAt,
-    updatedAt: blog.updatedAt,
+    featuredImage: blog.featuredImage,
   });
-  return { message: 'Blog Created successfully', user: newBlog };
+  return { message: 'Blog Created successfully',bog:newBlog };
 });
 
 
@@ -45,10 +56,35 @@ const getBlogs = catchAsync(async (req, res) => {
 });
 
 const getBlogByID = catchAsync(async (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const blogData = await db.select().from(blog).where(eq(blog.id, id));
-  return { blog:blogData };
+  if(blogData.length === 0) throw createError(404, `Blog with id ${id} not found`);
+  return { blog: blogData };
 });
 
+const deleteBlog = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const blogData = await db.delete(blog).where(eq(blog.id, id));
+  return { message:"Blog Deleted Successfully" };
+});
 
-module.exports = { createBlog, getBlogs,getBlogByID };
+async function uploadProductImage(req, res, next) {
+  try {
+    upload(req, res, async function (err) {
+      if (err) {
+        console.log(err);
+        return res.status(400).send('File has some error');
+      }
+      const file = req.file;
+      const data = {
+        file: file.filename,
+      };
+      console.log(file);
+      res.status(201).json({ status: 200, message: 'Product Image Created', image: file });
+    });
+  } catch (error) {
+    res.status(400).send('Upload Error');
+  }
+}
+
+module.exports = { createBlog, getBlogs, getBlogByID, uploadProductImage,deleteBlog };
--- app/routes/v1/blog.routes.js
+++ app/routes/v1/blog.routes.js
@@ -5,7 +5,10 @@ const { authenticateUser, authorizePermissions } = require('../../middleware');
 
 router.post('/', authenticateUser, authorizePermissions('admin'), controller.createBlog)
 router.get('/', controller.getBlogs)
+router.post('/upload', controller.uploadProductImage)
+
 router.get('/:id', controller.getBlogByID)
+router.delete('/:id', controller.deleteBlog)
 
 
 module.exports = router;
